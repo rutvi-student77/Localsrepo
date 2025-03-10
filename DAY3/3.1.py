3062b7e9caf9bbf0691a1ded8b8e71d6945cf29c
rutvi-student77 <bvarsani9999>
2025-03-10 16:18:28 +0530

    add data

---
+++ DAY3/3.1.py
@@ -0,0 +1,14 @@
+import math
+
+def calculate_distance(x1, y1, x2, y2):
+    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return distance
+
+x1 = float(input("Enter x1: "))
+y1 = float(input("Enter y1: "))
+x2 = float(input("Enter x2: "))
+y2 = float(input("Enter y2: "))
+
+distance = calculate_distance(x1, y1, x2, y2)
+
+print(f"The length of the line segment connecting the points ({x1}, {y1}) and ({x2}, {y2}) is: {distance}")
