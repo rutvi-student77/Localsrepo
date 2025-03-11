df370427dff9dc73fcfdaff49fb117413bd0c6dd
rutvi-student77 <bvarsani9999>
2025-03-11 16:12:32 +0530

    add data

--- Day4/4.2.py
+++ Day4/4.2.py
@@ -1,19 +1,22 @@
 "Create a function/input that counts the integer's number of digits.Solve this without using strings."
 
-def count_digits(number):
+def count(number):
+    number = abs(number)
+    
     if number == 0:
         return 1
-    
-    number = abs(number)
     
     digit_count = 0
     
     while number > 0:
         number //= 10  
-        digit_count += 1  
+        digit_count += 1 
     
     return digit_count
 
-number = 123456789
-result = count_digits(number)
-print(result)  
+print(count(318))      
+print(count(-925638))   
+print(count(4666))     
+print(count(-314890))  
+print(count(654321))   
+print(count(638476))  
