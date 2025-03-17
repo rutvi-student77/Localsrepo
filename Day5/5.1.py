8c72b02cd81a058dce95ad8710a19277bd5824fe
rutvi-student77 <bvarsani9999>
2025-03-17 15:40:08 +0530

    add data

---
+++ Day5/5.1.py
@@ -0,0 +1,12 @@
+"Create a function that takes an angle in radians and returns the corresponding angle in degrees rounded to one decimal place."
+import math
+
+def radians_to_degrees(radians):
+    degrees = radians * (180 / math.pi)
+    return round(degrees, 1)
+
+
+print(radians_to_degrees(1))
+print(radians_to_degrees(0))  
+print(radians_to_degrees(math.pi))  
+print(radians_to_degrees(math.pi / 2))  
